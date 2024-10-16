1afde0f3bf73badcde1cf0f64c401bcb812f07c9
grisha_ubica_rus76 <grisha2002grisha@mail.ru>
2024-10-16 21:54:42 +0300

    feature/task-1_multiplication_table_console_app

--- task1/src/main/java/Main.java
+++ task1/src/main/java/Main.java
@@ -2,70 +2,48 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void buildHeader(int maxValueCellSize, int tableSize){
-
-        for (int columnNumber = 0; columnNumber <= tableSize; columnNumber++) {
-            if (columnNumber != 0) {
-                if (columnNumber == tableSize) {
-                    //переменные
-                    System.out.printf("%" + (maxValueCellSize-1)+ "s", columnNumber);
-                } else {
-                    System.out.printf("%" + maxValueCellSize + "s", columnNumber + "|");
-                }
-            } else {
-                System.out.printf("%" + (maxValueCellSize-1) + "s", "|");
-            }
-        }
-
-
-    }
-
-    public static int initializeTableSize(){
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter table size: ");
-
-        int tableSize = scanner.nextInt();
-
-        if (tableSize > 32 || tableSize < 0) {
-            throw new RuntimeException("Invalid multiplication table size");
-        }
-
-        return tableSize;
-    }
-
     public static void main(String[] args) {
-
         int tableSize = initializeTableSize();
-
         int maxValueCellSize = String.valueOf(tableSize * tableSize * 10).length();
 
+        buildMultiplicationTable(maxValueCellSize, tableSize);
+    }
 
-
-        //header
-
-        buildHeader(maxValueCellSize,tableSize);
-
-        System.out.println();
+    public static void buildMultiplicationTable(int maxValueCellSize, int tableSize) {
+        buildHeader(maxValueCellSize, tableSize);
 
         String rowSeparator = buildRowSeparator(maxValueCellSize, tableSize);
 
         System.out.println(rowSeparator);
-        //header
-        for (int i = 1; i <= tableSize; i++) {
-            System.out.printf("%" + (maxValueCellSize - 1) + "s", i + "|");
-            for (int j = 1; j <= tableSize; j++) {
-                if (j == tableSize) {
-                    System.out.printf("%" + (maxValueCellSize - 1) + "s", (i * j));
+        for (int rowNumber = 1; rowNumber <= tableSize; rowNumber++) {
+            System.out.printf("%" + (maxValueCellSize - 1) + "s", rowNumber + "|");
+            for (int columnNumber = 1; columnNumber <= tableSize; columnNumber++) {
+                if (columnNumber == tableSize) {
+                    System.out.printf("%" + (maxValueCellSize - 1) + "s", (rowNumber * columnNumber));
                 } else {
-                    System.out.printf("%" + maxValueCellSize + "s", (i * j) + "|");
+                    System.out.printf("%" + maxValueCellSize + "s", (rowNumber * columnNumber) + "|");
                 }
             }
             System.out.println();
             System.out.println(rowSeparator);
         }
     }
-    //метод в мэйне только вызывается
+
+    public static void buildHeader(int maxValueCellSize, int tableSize) {
+        for (int columnNumber = 0; columnNumber <= tableSize; columnNumber++) {
+            if (columnNumber != 0) {
+                if (columnNumber == tableSize) {
+                    System.out.printf("%" + (maxValueCellSize - 1) + "s", columnNumber);
+                } else {
+                    System.out.printf("%" + maxValueCellSize + "s", columnNumber + "|");
+                }
+            } else {
+                System.out.printf("%" + (maxValueCellSize - 1) + "s", "|");
+            }
+        }
+        System.out.println();
+    }
+
     public static String buildRowSeparator(int maxValueCellSize, int tableSize) {
         StringBuilder builder = new StringBuilder();
         builder.append("-".repeat(maxValueCellSize - 2));
@@ -79,7 +57,22 @@ public class Main {
                 builder.append("-".repeat(maxValueCellSize - 1));
             }
         }
+
         return builder.toString();
     }
 
+    public static int initializeTableSize() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter table size: ");
+
+        int tableSize = scanner.nextInt();
+
+        if (tableSize > 32 || tableSize < 0) {
+            throw new RuntimeException("Invalid multiplication table size");
+        }
+
+        return tableSize;
+    }
 }
+
